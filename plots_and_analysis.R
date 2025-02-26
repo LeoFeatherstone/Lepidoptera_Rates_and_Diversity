92873634113fe20a1eeebbf5aef1fb8ad6ee7be7
LeoFeatherstone <leofeatherstone@protonmail.com>
2025-02-26 16:45:14 +1100

    add host info per species data

--- plots_and_analysis.R
+++ plots_and_analysis.R
@@ -16,7 +16,7 @@ species_data <- contrasts_host_species_data$species_data
 pairs_plot_genus <- contrasts %>%
     filter(dataset == "genera") %>%
     ungroup() %>%
-    select(-c("dataset", "label")) %>%
+    select(-c("dataset", "label", "weight", "n_species_left", "n_species_right", starts_with("n_host_record"))) %>%
     ggpairs() +
     labs(
         title = "Papilonoidea genera: log-transformed contrasts with pearson correlation",
@@ -27,7 +27,7 @@ pairs_plot_genus <- contrasts %>%
 pairs_plot_major_lineage <- contrasts %>%
     filter(dataset == "major-lineage") %>%
     ungroup() %>%
-    select(-c("dataset", "label", "dN_contrast", "dS_contrast", "baseml_bl_contrast")) %>%
+    select(-c("dataset", "label", "dN_contrast", "dS_contrast", "baseml_bl_contrast", "weight", "n_species_left", "n_species_right", starts_with("n_host_record"))) %>%
     ggpairs() +
     labs(
         title = "Lepidoptera major-lineage: log-transformed contrasts with pearson correlation",
@@ -38,7 +38,7 @@ pairs_plot_major_lineage <- contrasts %>%
 pairs_plot_family <- contrasts %>%
     filter(dataset == "family") %>%
     ungroup() %>%
-    select(-c("dataset", "label")) %>%
+    select(-c("dataset", "label", "weight", "n_species_left", "n_species_right", starts_with("n_host_record"))) %>%
     ggpairs() +
     labs(
         title = "Lepidoptera family: log-transformed contrasts with pearson correlation",
@@ -69,6 +69,7 @@ fit <- models %>%
 
 # Join model parameters into df
 species_dS_plot <- contrasts %>%
+    filter(dataset != "major-lineage") %>%
     left_join(fit, by = "dataset") %>%
     ggplot(aes(x = dS_contrast, y = n_species_contrast)) +
     geom_smooth(formula = y ~ x + 0, method= "lm", se = FALSE, col = alpha("black", 0.6)) +
@@ -92,4 +93,55 @@ species_dS_plot <- contrasts %>%
 ggsave(
     plot = species_dS_plot, filename = "species_vs_dS.pdf",
     width = 6, height = 3, units = "in"
+)
+
+# N species in taxa vs number of host records
+host_vs_species_plot <- species_data %>%
+    ggplot(aes(x = n_species, y = n_host_record)) +
+    geom_point(shape = 21, fill = alpha("dodgerblue", 0.7)) +
+    facet_wrap(
+        ~level, scales = "free",
+        labeller = as_labeller(c(family = "LepIndex families", genus = "LepIndex genera"))
+    ) +
+    labs(x = "Number of species in family/genera", y = "Number with host record") +
+    theme(
+        text = element_text(size = 12)
+    )
+
+ggsave(
+    plot = host_vs_species_plot, filename = "host_vs_species.pdf",
+    width = 6, height = 3, units = "in"
+)
+
+# Prepare lavaan plot code
+library(lavaan)
+library(semPlot)
+library(gridExtra)
+
+# Define the path model
+path_model <- '
+    n_species_contrast ~ dS_contrast + dN_contrast + baseml_bl_contrast
+    dS_contrast ~ dN_contrast + baseml_bl_contrast
+    dN_contrast ~ baseml_bl_contrast
+'
+
+# Fit the model for each group
+fit_models <- contrasts %>%
+    group_by(dataset) %>%
+    do(fit = sem(path_model, data = .))
+
+# Plot the path models
+plot_list <- fit_models %>%
+    rowwise() %>%
+    mutate(plot = list(semPaths(fit, "std", layout = "circle", title = dataset))) %>%
+    pull(plot)
+
+# Arrange plots side by side
+grid.arrange(grobs = plot_list, ncol = 2)
+
+# Save the combined plot
+ggsave(
+    filename = "path_models_combined.pdf",
+    plot = grid.arrange(grobs = plot_list, ncol = 2),
+    width = 16, height = 8, units = "in"
 )
--- wrangle_genera_family.R
+++ wrangle_genera_family.R
@@ -17,10 +17,10 @@ extract_paml_lengths <- function(tree, measure) {
     right_taxa <- pair[2]
 
     terminal_edges <- tree$edge.length[tree$edge[, 2] <= Ntip(tree)]
-    
+
     left_blen <- terminal_edges[tree$tip.label == left_taxa]
     right_blen <- terminal_edges[tree$tip.label == right_taxa]
-    
+
     result <- setNames(
         data.frame(
             left_taxa, left_blen,
@@ -60,38 +60,7 @@ extract_family <- function(name) {
 }
 ## End convenience functions
 
-## Begin prepare species diversity data
-family_lepindex <- read_csv("Data/Papi_Genera/Lepi_Families_Lepindex_Raw.csv") %>%
-    filter(!if_all(everything(), ~ . == "")) # Remove weird empty rows
-papilionoidea_lepindex <- read_csv("Data/Papi_Genera/Papi_Genera_Lepindex_Raw.csv") %>%
-    filter(!if_all(everything(), ~ . == ""))
-
-species_data <- bind_rows(family_lepindex, papilionoidea_lepindex) %>%
-    rename_with(tolower) %>%
-    filter(str_detect(name_data, "Valid Name")) %>%
-    distinct(name_data, .keep_all = T)
-
-species_counts_genus <- species_data %>%
-    group_by(genus) %>% 
-    summarise(n_species = n()) %>% 
-    mutate(genus = str_to_title(genus)) %>% 
-    select(genus, n_species) %>%
-    drop_na()
-
-species_counts_family <- species_data %>%
-    group_by(family) %>% 
-    summarise(n_species = n()) %>% 
-    mutate(family = str_to_title(family)) %>% 
-    select(family, n_species) %>%
-    drop_na()
-
-combined_species_counts <- bind_rows(species_counts_genus, species_counts_family) %>%
-    rowwise() %>%
-    mutate(taxonomic_group = ifelse(is.na(genus), family, genus)) %>%
-    select(taxonomic_group, n_species)
-## End preparing species diversity data
-
-# Begin prepare host counts
+## Begin prepare host counts
 host_datasets <- c(
     "Data/Papi_Genera/Papi_Genera_HOSTS.csv",
     "Data/Lepi_MajorLineages/Lepi_MajorLineages_HOSTS.csv",
@@ -100,7 +69,7 @@ host_datasets <- c(
 
 host_data <- lapply(host_datasets, read_csv) %>%
     bind_rows() %>%
-    filter(!if_all(everything(), ~ . == "")) %>% 
+    filter(!if_all(everything(), ~ . == "")) %>%
     rename_with(tolower) %>%
     mutate(
         genus = str_to_title(str_trim(genus)),
@@ -151,6 +120,62 @@ combined_host_counts <- bind_rows(host_counts_genus, host_counts_family) %>%
     select(taxonomic_group, prop_generalist, n_host_families, n_host_species)
 ## End prepare hosts counts
 
+## Begin prepare species diversity data
+family_lepindex <- read_csv("Data/Papi_Genera/Lepi_Families_Lepindex_Raw.csv") %>%
+    filter(!if_all(everything(), ~ . == "")) # Remove weird empty rows
+papilionoidea_lepindex <- read_csv("Data/Papi_Genera/Papi_Genera_Lepindex_Raw.csv") %>%
+    filter(!if_all(everything(), ~ . == ""))
+major_lineage_lepindex <- read_csv("Data/Lepi_MajorLineages/Lepi_MajorLineages_Lepindex.csv") %>%
+    select(Genus, Name_data) %>% # Avoid first number column
+    filter(!if_all(everything(), ~ . == ""))
+
+species_data <- bind_rows(family_lepindex, papilionoidea_lepindex, major_lineage_lepindex) %>%
+    rename_with(tolower) %>%
+    rowwise() %>%
+    filter(str_detect(name_data, "Valid Name")) %>%
+    distinct(name_data, .keep_all = T) %>% # TODO: Should I take distince name_data or species?
+    mutate(species = word(name_data, 1)) %>%
+    filter(genus != species) %>% # When genus matches species (avoid n() - 1 in original code)
+    mutate(
+        species = str_to_lower(species),
+        genus = str_to_title(genus),
+        family = str_to_title(family)
+    ) %>%
+    mutate(species = paste(genus, species)) %>%
+    # Drop rows with NA family and genus, species, name_data identical in another row
+    group_by(genus, species, name_data) %>%
+    filter(!(is.na(family) & n() > 1)) %>%
+    ungroup() %>%
+    # Record species with host in host_data
+    left_join(
+        host_data %>% select(species) %>% distinct() %>% mutate(host_record = 1),
+        by = "species"
+    ) %>%
+    mutate(host_record = replace_na(host_record, 0))
+
+species_counts_genus <- species_data %>%
+    group_by(genus) %>%
+    summarise(
+        n_species = n_distinct(species),
+        n_host_record = sum(host_record)
+    ) %>%
+    drop_na()
+
+species_counts_family <- species_data %>%
+    group_by(family) %>%
+    summarise(
+        n_species = n_distinct(species),
+        n_host_record = sum(host_record)
+    ) %>%
+    drop_na()
+
+combined_species_counts <- bind_rows(species_counts_genus, species_counts_family) %>%
+    rowwise() %>%
+    mutate(taxonomic_group = ifelse(is.na(genus), family, genus)) %>%
+    mutate(level = ifelse(is.na(genus), "family", "genus")) %>%
+    select(level, taxonomic_group, n_species, n_host_record)
+## End preparing species diversity data
+
 ## Begin parse PAML output as lists of trees for each measure (baseml_bl, dN, dS) and major lineagge data
 baseml_trees_files <- list.files(
     path = c("PAML_outputs/Papi_Genera", "PAML_outputs/Lepi_Families"),
@@ -198,7 +223,7 @@ major_lineage_data <- read_csv("Raw_Sister_Tables/Lepidoptera_MajorLineages_Cont
         cols = ends_with("_left") | ends_with("_right"),
         names_to = c(".value", "side"),
         names_pattern = "(.*)_(left|right)"
-    ) 
+    )
 
 ## End parse PAML output as lists of trees for each measure (baseml_bl, dN, dS) and major lineagge data
 
@@ -210,18 +235,25 @@ pair_data_long <- baseml_bl_df %>%
         cols = ends_with("_left") | ends_with("_right"),
         names_to = c(".value", "side"),
         names_pattern = "(.*)_(left|right)"
-    ) %>% 
+    ) %>%
     rowwise() %>%
     mutate(
         taxonomic_group = ifelse(dataset == "family", extract_family(taxa), extract_genus(taxa))
     ) %>%
     mutate(taxonomic_group = ifelse(taxonomic_group == "NS0181", "Setabis", taxonomic_group))
 
-# Pivot left-right wider for contrasts and left join major_lineage_data
+# Pivot left-right wider for contrasts and left join major_lineage_data TODO: Add weight
 pair_data_wide <- pair_data_long %>%
-    left_join(combined_species_counts, by = "taxonomic_group") %>%
+    left_join(
+        combined_species_counts %>% select(-level),
+        by = "taxonomic_group"
+    ) %>%
     left_join(combined_host_counts, by = "taxonomic_group") %>%
     bind_rows(major_lineage_data) %>%
+    # weighting
+    mutate(
+        inv_weight = (n_species - n_host_record) / (n_host_record * (n_species - 1))
+    ) %>%
     group_by(dataset, pair_id) %>%
     pivot_wider(
         names_from = side,
@@ -235,21 +267,22 @@ pair_data_wide <- pair_data_long %>%
 contrasts <- pair_data_wide %>%
     group_by(dataset) %>%
     mutate(label = paste(taxonomic_group_left, "-", taxonomic_group_right)) %>%
-    mutate(across(
-        .cols = -c(
-            pair_id, taxa_left, taxa_right,
-            taxonomic_group_left, taxonomic_group_right, label,
-            prop_generalist_left, prop_generalist_right
-        ),
-        .fns = log
-    )) %>%
     mutate(
-        sign = sign(n_species_left - n_species_right)
+        sign = sign(n_species_left - n_species_right),
+        weight = 1 / (inv_weight_left + inv_weight_right)
     ) %>%
+    select(-starts_with("inv_weight")) %>%
     mutate(
         prop_generalist_left = asin(sqrt(prop_generalist_left)),
         prop_generalist_right = asin(sqrt(prop_generalist_right))
     ) %>%
+    mutate(across(
+        .cols = -c(
+            pair_id, label, sign, weight,
+            starts_with(c("prop_generalist", "n_host_record", "taxonomic", "taxa_"))
+        ),
+        .fns = log
+    )) %>%
     mutate(
         dN_contrast = sign * (dN_left - dN_right),
         dS_contrast = sign * (dS_left - dS_right),
@@ -259,7 +292,9 @@ contrasts <- pair_data_wide %>%
         baseml_bl_contrast = sign * (baseml_bl_left - baseml_bl_right),
         prop_generalist_contrast = sign * (prop_generalist_left - prop_generalist_right)
     ) %>%
-    select(c(ends_with("contrast"), "label"))
+    select(c(
+        ends_with("contrast"), starts_with(c("n_host_record", "n_species")), "label", "weight"
+    ))
 
 saveRDS(
     list(
